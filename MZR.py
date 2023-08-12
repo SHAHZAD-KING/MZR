dbeb82e8bf1a2397c7f8dc0ddc240fb8b47c92d9
SHAHZAD-KING <134486445+shahzadrahimy@users.noreply.github.com>
2023-08-12 18:18:14 +0430

    Update MZR.py
    rm -rf MZR
    
    git clone https://github.com/shahzadrahimy/MZR
    
    cd MZR
    
    python MZR.py

--- MZR.py
+++ MZR.py
@@ -3,6 +3,6 @@ import sys
 import time
 os.system("clear")
 os.system("git pull")
-print('\033[1;31[×] JOIN MY TELGRAM CHANNEL ');time.sleep(3)
+print('\033[1;31m[×] JOIN MY TELGRAM CHANNEL ');time.sleep(3)
 os.system('xdg-open https://t.me/SKING_TECH')
 import MAZAR
